94196d873c7a227a1ba435f83bd30e54b2d0baf6
Juha Sonck <juha.sonck@pointcollege.fi>
2020-10-08 12:42:31 +0300

    Kopioitu aikaisempi projekti pohjaksi uudelle IoT-palvelinprojektille

---
+++ PointCollegeSensorControl/Controllers/CommandsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CareeriaIOTSensorControl.Models;
+
+namespace CareeriaIOTSensorControl.Controllers
+{
+    public class CommandsController : Controller
+    {
+        private IoTDBEntities db = new IoTDBEntities();
+
+        // GET: Commands
+        public ActionResult Index()
+        {
+            return View(db.Commands.ToList());
+        }
+
+        public string GetCommand(int id)
+        {
+            string command = (from c in db.Commands
+                              where (c.DeviceId == id) && (c.Executed == false)
+                              select c.Command).FirstOrDefault();
+            if (command == null)
+                command = "";
+            return command;
+        }
+
+        public string Completed(int id)
+        {
+            Commands command = (from c in db.Commands
+                              where (c.DeviceId == id) && (c.Executed == false)
+                              select c).FirstOrDefault();
+            if (command != null)
+            {
+                command.Executed = true;
+                db.SaveChanges();
+            }
+            return "Ok";
+        }
+
+
+
+        // GET: Commands/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commands commands = db.Commands.Find(id);
+            if (commands == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commands);
+        }
+
+        // GET: Commands/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Commands/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Command,DeviceId,Command")] Commands commands)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Commands.Add(commands);
+                commands.Executed = false;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(commands);
+        }
+
+        // GET: Commands/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commands commands = db.Commands.Find(id);
+            if (commands == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commands);
+        }
+
+        // POST: Commands/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Command,DeviceId,Command,Executed")] Commands commands)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(commands).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(commands);
+        }
+
+        // GET: Commands/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Commands commands = db.Commands.Find(id);
+            if (commands == null)
+            {
+                return HttpNotFound();
+            }
+            return View(commands);
+        }
+
+        // POST: Commands/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Commands commands = db.Commands.Find(id);
+            db.Commands.Remove(commands);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PointCollegeSensorControl/Controllers/MeasurementsController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CareeriaIOTSensorControl.Models;
+using System.Globalization;
+
+namespace CareeriaIOTSensorControl.Controllers
+{
+    //8.11.2016 Lisätty koodi, joka tallentaa tietokantaan:
+    public class MeasurementsController : Controller
+    {
+        private IoTDBEntities db = new IoTDBEntities();
+
+        public String Store(int id, string temp)
+        {
+            measurements m = new measurements();
+            m.sender = id;
+            CultureInfo en = new CultureInfo("en-US");
+            m.value = double.Parse(temp, en);
+            m.time = DateTime.Now;
+            m.type = 1;
+            db.measurements.Add(m);
+            db.SaveChanges();
+            return "ok";
+        } 
+
+        // GET: Measurements
+        public ActionResult Index()
+        {
+            var mittaukset = (from m in db.measurements
+                              orderby m.time descending
+                              select m).Take(500).ToList();
+            return View(mittaukset);
+        }
+
+        // GET: Measurements/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            measurements measurements = db.measurements.Find(id);
+            if (measurements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measurements);
+        }
+
+        // GET: Measurements/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Measurements/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,sender,time,type,value")] measurements measurements)
+        {
+            if (ModelState.IsValid)
+            {
+                db.measurements.Add(measurements);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(measurements);
+        }
+
+        // GET: Measurements/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            measurements measurements = db.measurements.Find(id);
+            if (measurements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measurements);
+        }
+
+        // POST: Measurements/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,sender,time,type,value")] measurements measurements)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(measurements).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(measurements);
+        }
+
+        // GET: Measurements/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            measurements measurements = db.measurements.Find(id);
+            if (measurements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(measurements);
+        }
+
+        // POST: Measurements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            measurements measurements = db.measurements.Find(id);
+            db.measurements.Remove(measurements);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
