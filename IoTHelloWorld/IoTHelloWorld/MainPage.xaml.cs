7018e9a9139801128674b491adfa9a546db999ee
Juha Sonck <juha.sonck@pointcollege.fi>
2020-10-16 15:07:06 +0300

    Aloitettu UWP-sovelluksen kehittäminen ja otettu emmelsoftin kirjastot käyttöön

---
+++ IoTHelloWorld/IoTHelloWorld/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel.Channels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IoTHelloWorld
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnHello_Click(object sender, RoutedEventArgs e)
+        {
+            message.Text = "Heijaa Porvoo!";
+        }
+
+        private void BtnSulje_Click(object sender, RoutedEventArgs e)
+        {
+            message.Text = "Suljen sovelluksen";
+            //CoreApplication.Exit();
+            Application.Current.Exit();
+        }
+    }
+}
