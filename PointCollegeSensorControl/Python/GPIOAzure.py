d792e18762aefec52efb4fed69e0949e443836b2
Juha Sonck <juha.sonck@pointcollege.fi>
2020-10-23 14:25:54 +0300

    Lisätty laitaid:n haku tiedostosta

--- PointCollegeSensorControl/Python/GPIOAzure.py
+++ PointCollegeSensorControl/Python/GPIOAzure.py
@@ -2,14 +2,20 @@ from gpiozero import LED
 from time import sleep
 import urllib.request
 import RPi.GPIO as GPIO
-
+#GPIO-modulin asetukset
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(20,GPIO.OUT)
 GPIO.setup(16,GPIO.OUT)
-# muuta tähän oman Raspberry-laitteesi id-numero!
-deviceid=1
-
+# haetaan Raspberry-laitteen id-numero device.dat -nimisestä tiedostosta
+df = open("device.dat","r")
+deviceid = df.read()
+if (deviceid != ''):
+    print("Löysin laitekoodin tiedostosta")
+# ellei sitä löydy käytetään ohjelmaan kovakoodattua arvoa
+else:
+    deviceid=1
+print("Käytän laitekoodia: " + deviceid)
 while True:
     url = "http://careeriawebappiot.azurewebsites.net/commands/getcommand/"+str(deviceid)
     print(url)
