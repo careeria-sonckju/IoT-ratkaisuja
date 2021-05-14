c91846109e97b6c982e13ee7ce05a69625a50f2c
ASUSROGSONCK\JuhaAdmin <juha.sonck@careeria.fi>
2021-05-14 11:05:19 +0300

    Korjattu Azure-komentoa

--- PointCollegeSensorControl/Python/temper_azure_python.py
+++ PointCollegeSensorControl/Python/temper_azure_python.py
@@ -4,10 +4,14 @@ import urllib.request
 
 ap = SenseHat()
 
+# muuta tähän oman Raspberry-laitteesi id-numero!
+deviceid=8
+
 while True:
+    type=1
     temp = ap.get_temperature()-12
     print("Temp: %s C" % temp)
-    url = "http://careeriawebappiot.azurewebsites.net/measurements/store/1?temp="+str(temp)
+    url = "http://careeriawebappiot.azurewebsites.net/measurements/store/"+str(deviceid)+"?value="+str(temp)+"&type="+str(type)
     print(url)
     htmlfile = urllib.request.urlopen(url)
     htmltext = htmlfile.read()
@@ -17,3 +21,4 @@ while True:
 
     ap.show_message("%.1f C" % temp, scroll_speed=0.12, text_colour=[255, 255, 255])
     time.sleep(5)
+
