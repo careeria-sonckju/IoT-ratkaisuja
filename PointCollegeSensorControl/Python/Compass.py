6394f84a793f9a73ff6a663a969a870e8592c76c
ASUSROGSONCK\JuhaAdmin <juha.sonck@careeria.fi>
2021-05-14 13:09:13 +0300

    kompassia muokattu

--- PointCollegeSensorControl/Python/Compass.py
+++ PointCollegeSensorControl/Python/Compass.py
@@ -1,47 +1,29 @@
-#from sense_hat import SenseHat
-
-#sense = SenseHat()
-
-#red = (255, 0, 0)
-#blue = (0, 0, 255)
-
-#while True:
-#    north = sense.get_compass()
-#    print("North: %s" % north)
-#!/usr/bin/python
-import sys
 from sense_hat import SenseHat
 
-# To get good results with the magnetometer you must first calibrate it using
-# the program in RTIMULib/Linux/RTIMULibCal
-# The calibration program will produce the file RTIMULib.ini
-# Copy it into the same folder as your Python code
-
-led_loop = [4, 5, 6, 7, 15, 23, 31, 39, 47, 55, 63, 62, 61, 60, 59, 58, 57, 56, 48, 40, 32, 24, 16, 8, 0, 1, 2, 3]
-
 sense = SenseHat()
-sense.set_rotation(0)
-sense.clear()
 
-prev_x = 0
-prev_y = 0
-
-led_degree_ratio = len(led_loop) / 360.0
+red = (255, 0, 0)
+blue = (0, 0, 255)
 
 while True:
-    dir = sense.get_compass()
-    dir_inverted = 360 - dir  # So LED appears to follow North
-    led_index = int(led_degree_ratio * dir_inverted)
-    offset = led_loop[led_index]
+    north = sense.get_compass()
+    print("North: %s" % north)
 
-    y = offset // 8  # row
-    x = offset % 8  # column
-
-    if x != prev_x or y != prev_y:
-        sense.set_pixel(prev_x, prev_y, 0, 0, 0)
-
-    sense.set_pixel(x, y, 0, 0, 255)
-
-    prev_x = x
-    prev_y = y
-
+    from sense_hat import SenseHat
+sense = SenseHat()
+#Get the direction of North in degrees (compared to the USB ports direction)
+#360/0° is North, 90 is East, 270 is West and 180 is South
+direction = sense.get_compass()
+print("direction: %s" % direction)
+if (direction > 0 && dirction < 45):
+    print("Pohjoinen")
+elif (direction >= 45 && direction < 135):
+    print("Itä")
+elif (direction >= 135 && direction < 215):
+    print("Etelä")
+elif (direction >= 215 && direction < 305):
+    print("Länsi")
+elif (direction >= 305 && direction < 360):
+    print("Pohjoinen360")
+else:
+    print("suunta tuntematon")
