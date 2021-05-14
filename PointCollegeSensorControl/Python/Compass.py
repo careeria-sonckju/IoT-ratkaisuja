14912c115d07eacc8ecb1a2a6c763a50f76e4de4
ASUSROGSONCK\JuhaAdmin <juha.sonck@careeria.fi>
2021-05-14 13:11:13 +0300

    korjattu if - and

--- PointCollegeSensorControl/Python/Compass.py
+++ PointCollegeSensorControl/Python/Compass.py
@@ -15,15 +15,15 @@ sense = SenseHat()
 #360/0° is North, 90 is East, 270 is West and 180 is South
 direction = sense.get_compass()
 print("direction: %s" % direction)
-if (direction > 0 && dirction < 45):
+if (direction > 0 and dirction < 45):
     print("Pohjoinen")
-elif (direction >= 45 && direction < 135):
+elif (direction >= 45 and direction < 135):
     print("Itä")
-elif (direction >= 135 && direction < 215):
+elif (direction >= 135 and direction < 215):
     print("Etelä")
-elif (direction >= 215 && direction < 305):
+elif (direction >= 215 and direction < 305):
     print("Länsi")
-elif (direction >= 305 && direction < 360):
+elif (direction >= 305 and direction < 360):
     print("Pohjoinen360")
 else:
     print("suunta tuntematon")
