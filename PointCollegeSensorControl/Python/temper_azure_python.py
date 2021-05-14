d2fcf4d67cb7f195e4f09f6cd3f1ce5041a82a9c
ASUSROGSONCK\JuhaAdmin <juha.sonck@careeria.fi>
2021-05-14 11:08:57 +0300

    muutettu sensehatin objeksin nimi yhteneväiseksi

--- PointCollegeSensorControl/Python/temper_azure_python.py
+++ PointCollegeSensorControl/Python/temper_azure_python.py
@@ -2,23 +2,22 @@ from sense_hat import SenseHat
 import time
 import urllib.request
 
-ap = SenseHat()
+sense = SenseHat()
 
 # muuta tähän oman Raspberry-laitteesi id-numero!
 deviceid=8
 
 while True:
     type=1
-    temp = ap.get_temperature()-12
+    temp = sense.get_temperature()-12
     print("Temp: %s C" % temp)
-    url = "http://careeriawebappiot.azurewebsites.net/measurements/store/"+str(deviceid)+"?value="+str(temp)+"&type="+str(type)
+    url = "http://careeriawebsensepiot.azurewebsites.net/measurements/store/"+str(deviceid)+"?value="+str(temp)+"&type="+str(type)
     print(url)
     htmlfile = urllib.request.urlopen(url)
     htmltext = htmlfile.read()
     print(htmltext)
 
-    ap.set_rotation(180)        
+    sense.set_rotation(180)        
 
-    ap.show_message("%.1f C" % temp, scroll_speed=0.12, text_colour=[255, 255, 255])
+    sense.show_message("%.1f C" % temp, scroll_speed=0.12, text_colour=[255, 255, 255])
     time.sleep(5)
-
