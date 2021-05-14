d992c0c0805bcc4404cf30a06e68c09392f84012
ASUSROGSONCK\JuhaAdmin <juha.sonck@careeria.fi>
2021-05-14 12:13:33 +0300

    muokattu

--- PointCollegeSensorControl/Python/Acceleration.py
+++ PointCollegeSensorControl/Python/Acceleration.py
@@ -1,4 +1,8 @@
 from sense_hat import SenseHat
+import time
 sense = SenseHat()
-accel_only = sense.get_accelerometer()
-print("p: {pitch}, r: {roll}, y: {yaw}".format(**accel_only))
+
+while True:
+    accel_only = sense.get_accelerometer()
+    print("p: {pitch}, r: {roll}, y: {yaw}".format(**accel_only))
+    time.sleep(1)
