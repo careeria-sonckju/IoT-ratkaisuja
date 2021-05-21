a0c94b3bd2bb52a095b53d66bc0c5e9e697a38da
careeria-sonckju <49709746+careeria-sonckju@users.noreply.github.com>
2021-05-21 10:17:34 +0300

    Add files via upload
    Muokattu prosessikäsittelyn esimerkkjeä

---
+++ PointCollegeSensorControl/Python/ProsessiEsimerkki2-ikisilmukka.py
@@ -0,0 +1,32 @@
+import multiprocessing
+import time
+
+def funktio1():
+    name = multiprocessing.current_process().name
+    print (name, 'is starting')
+    time.sleep(1)
+    print (name, 'Exiting')
+    time.sleep(1)
+    return
+    
+
+def funktio2():
+    while True:
+        name = multiprocessing.current_process().name
+        print (name, 'is starting')
+        time.sleep(1)
+        print (name, 'Exiting')
+        time.sleep(1)
+    
+
+if __name__ == '__main__':
+    service1 = multiprocessing.Process(name='LÄMPÖTILANMITTAUS', target=funktio1)
+    service2 = multiprocessing.Process(name='KOSTEUSMITTAUS', target=funktio2)
+    service3 = multiprocessing.Process(name='ILMANPAINEENMITTAUS', target=funktio1)
+    #service3 = multiprocessing.Process(target=funktio1) # use default name
+
+    service3.start() #ILMANPAINEENMITTAUS
+    time.sleep(1)
+    service2.start() #KOSTEUSMITTAUS
+    time.sleep(1)
+    service1.start() #LÄMPÖTILANMITTAUS
