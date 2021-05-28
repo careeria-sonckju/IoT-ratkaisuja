1f773865c3caaaa7800ac93edb203c3c4b01f05b
ASUSROGSONCK\JuhaAdmin <juha.sonck@careeria.fi>
2021-05-28 10:19:37 +0300

    Lisätty uusi button-ohjelma

--- PointCollegeSensorControl/Python/buttonTest.py
+++ PointCollegeSensorControl/Python/buttonTest.py
@@ -1,11 +1,9 @@
-from gpiozero import Button
-from time import sleep
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO -kirjasto
 
-button = Button(2)
+GPIO.setwarnings(False) #Ignoroidaan mahdolliset virheet
+GPIO.setmode(GPIO.BOARD) #Ei käytetä BCM-pinnijärjestystä, vaan fyysistä!
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Asettaa pin10:n input pin:ksi ja asettaa alkuarvoksi = "pulled down" eli pois päältä
 
-while True:
-    if button.is_pressed:
-        print("Pressed")
-    else:
-        print("Released")
-    sleep(1)
+while True: #ikiluuppi
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pressed!")
---
+++ PointCollegeSensorControl/Python/buttonTest2.py
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CareeriaIOTSensorControl.Python
+{
+	public class buttonTest2
+	{
+	}
+}
