29140befaa4cb9644ac245df8c60cf67d7139f0e
ASUSROGSONCK\JuhaAdmin <juha.sonck@careeria.fi>
2021-06-03 18:57:37 +0300

    sekoiliee

--- PointCollegeSensorControl/Python/etaisyysmittausKirjastonAvulla.py
+++ PointCollegeSensorControl/Python/etaisyysmittausKirjastonAvulla.py
@@ -1,20 +1,15 @@
 ﻿from multiprocessing import Process
 from Bluetin_Echo import Echo
+import RPi.GPIO as GPIO
 import time
-
-TRIGGER_PIN = 23
-ECHO_PIN = 24
-
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM) #Käytetään BCM-mäppäystä pinnien hallintaan
+TRIGGER_PIN = 23 #Liipasimen BCM-pinni on 23
+ECHO_PIN = 24 #Äänen kaiun BCM-pinni on 24
 GPIO.setwarnings(False)
 def distanceMeasure():
     while True:
         speed_of_sound = 340
         echo = Echo(TRIGGER_PIN, ECHO_PIN, speed_of_sound)
-
-        result = echo.read('cm', 1)
-        # Print result.
-        print(result, 'cm')
 
         # Number of distance measurements to average.
         samples = 5
@@ -29,8 +24,7 @@ def main():
     p1 = Process(target=distanceMeasure, args=())
     p1.start()
     #p1.join()
-    print('pääohjelma päättyi')
+    print('pääohjelma suoritettu')
 
 if __name__ == '__main__':
     main()
-
