522923ca659010080a840672a9b748bf47a08bc7
careeria-sonckju <49709746+careeria-sonckju@users.noreply.github.com>
2021-09-09 15:25:11 +0300

    Add files via upload
    Muokattu kurssimaterialia varten

--- PointCollegeSensorControl/Python/rfid/writeloop.py
+++ PointCollegeSensorControl/Python/rfid/writeloop.py
@@ -4,13 +4,13 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
-text = input('New data:')
+text = input('Type new data to be stored in RFID-memory:')
 laskuri=0
 while True:
     try:
-            laskuri = laskuri + 1
-            print("Now place your tag " + str(laskuri) + " to write")
-            reader.write(text+str(laskuri))
-            print("Written")
+        laskuri = laskuri + 1
+        print("Now place your tag " + str(laskuri) + " to write")
+        reader.write(text+str(laskuri))
+        print("Written")
     finally:
-            GPIO.cleanup()
+        GPIO.cleanup()
