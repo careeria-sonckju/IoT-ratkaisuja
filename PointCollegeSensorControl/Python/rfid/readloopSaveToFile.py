50bc1489b34cbe93d779238897a1843da250c060
careeria-sonckju <49709746+careeria-sonckju@users.noreply.github.com>
2021-09-30 16:22:06 +0300

    kopioitu rfid-kehitettyjä tiedostoja
    kopioitu rfid-kehitettyjä tiedostoja oikeaan alikansioon

--- PointCollegeSensorControl/Python/rfid/readloopSaveToFile.py
+++ PointCollegeSensorControl/Python/rfid/readloopSaveToFile.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+import os
 GPIO.setwarnings(False)
 reader = SimpleMFRC522()
 i=0
 try:
+    name = "RFID-kortteja"
+    file = open("cards.txt", "w")
+    file.write(name+"\n")
+
     while True:
+        file = open("cards.txt", "a")
         i=i+1
         id, text = reader.read()
         tagi=str(id)
-        print("Kierros = "+str(i))
+        file.write(tagi+"\n")
+        print("Kierros = "+str(i)+"\n")
         print(tagi)
         print(text)
-
+        
 finally:
     file.close()
     GPIO.cleanup()
